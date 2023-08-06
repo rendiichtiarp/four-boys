10ace8f6cf162ee31a365602a24b109cc7cd9658
Rendi Ichtiar Prasetyo <90301690+rendiichtiarp@users.noreply.github.com>
2023-08-06 21:51:18 +0700

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/styles/tailwind.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
 import { Inter, Lora } from "next/font/google";
+import Maintenance from "@/components/maintenance";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -13,6 +14,8 @@ const lora = Lora({
   variable: "--font-lora"
 });
 
+const isMaintenanceMode = true;
+
 export default function RootLayout({
   children
 }: {
@@ -22,9 +25,14 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cx(inter.variable, lora.variable)}>
+      className={cx(inter.variable, lora.variable)}
+    >
       <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
-        <Providers>{children}</Providers>
+        {isMaintenanceMode ? (
+          <Maintenance />
+        ) : (
+          <Providers>{children}</Providers>
+        )}
       </body>
     </html>
   );
